c185ede7a1be7594dd2b5f4ac31541cff717d194
Damjan Stojchev <damjan.stojcev4@gmail.com>
2023-10-28 15:58:46 +0200

    Modified its field from name to category

--- Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/model/Category.java
+++ Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/model/Category.java
@@ -16,7 +16,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String category;
 
     @ManyToMany(mappedBy = "categories")
     private Set<Book> books;
