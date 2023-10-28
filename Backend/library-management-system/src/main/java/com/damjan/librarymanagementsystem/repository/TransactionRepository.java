c6707e0fd2b834be3f3e4d6dac4ce0706e829e7d
Damjan Stojchev <damjan.stojcev4@gmail.com>
2023-10-28 18:11:45 +0200

    Transactions Repository

---
+++ Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/repository/TransactionRepository.java
@@ -0,0 +1,34 @@
+package com.damjan.librarymanagementsystem.repository;
+
+import com.damjan.librarymanagementsystem.model.Book;
+import com.damjan.librarymanagementsystem.model.Transaction;
+import com.damjan.librarymanagementsystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    //This is useful for showing a user's transaction history.
+    List<Transaction> findTransactionsByUser(User user);
+
+    // This can be useful for tracking late returns and calculating late fees
+    List<Transaction> findOverdueTransactions();
+
+    // find all transactions related to a specific book
+    List<Transaction> findTransactionsByBook(Book book);
+
+    // Active Transactions where book is currently checked out
+    List<Transaction> findActiveTransactions();
+
+    //  find all transactions associated with a specific user and a SPECIFIC BOOK
+    List<Transaction> findTransactionsByUserAndBook(User user, Book book);
+
+    // calculate total fees
+    @Query("SELECT SUM(t.lateFee) FROM Transaction t WHERE t.user = :user AND t.status = 'OVERDUE'")
+    Double calculateTotalFinesForUser(@Param("user") User user);
+
+}
