127e8fd5f35eee738852111c9261adafa2536bfa
Damjan Stojchev <damjan.stojcev4@gmail.com>
2023-10-28 14:53:56 +0200

    User Model

---
+++ Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/model/User.java
@@ -0,0 +1,32 @@
+package com.damjan.librarymanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Transaction> transactions;
+
+}
