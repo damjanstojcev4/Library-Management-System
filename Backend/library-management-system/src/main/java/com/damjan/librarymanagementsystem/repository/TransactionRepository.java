8582fee4be463ebaa2ca5385aaa74c96af7fed94
Damjan Stojchev <damjan.stojcev4@gmail.com>
2023-11-07 18:56:34 +0100

    Added custom queries

--- Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/repository/TransactionRepository.java
+++ Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/repository/TransactionRepository.java
@@ -16,12 +16,15 @@ public interface TransactionRepository extends JpaRepository<Transaction, Intege
     List<Transaction> findTransactionsByUser(User user);
 
     // This can be useful for tracking late returns and calculating late fees
+    @Query("SELECT t FROM Transaction t WHERE t.status = 'Overdue' AND t.checkIn > t.dueDate")
     List<Transaction> findOverdueTransactions();
+
 
     // find all transactions related to a specific book
     List<Transaction> findTransactionsByBook(Book book);
 
     // Active Transactions where book is currently checked out
+    @Query("SELECT t FROM Transaction t WHERE t.status = 'CHECKED_OUT' AND t.dueDate >= CURRENT_TIMESTAMP")
     List<Transaction> findActiveTransactions();
 
     //  find all transactions associated with a specific user and a SPECIFIC BOOK
