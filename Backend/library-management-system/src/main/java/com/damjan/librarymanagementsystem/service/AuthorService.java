e1b2e6fc0cf78ad15d390b07901a2e87cec5abe0
Damjan Stojchev <damjan.stojcev4@gmail.com>
2023-11-09 21:24:47 +0100

    Almost finished AuthorService

--- Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/service/AuthorService.java
+++ Backend/library-management-system/src/main/java/com/damjan/librarymanagementsystem/service/AuthorService.java
@@ -1,14 +1,25 @@
 package com.damjan.librarymanagementsystem.service;
 
 import com.damjan.librarymanagementsystem.model.Author;
+import com.damjan.librarymanagementsystem.model.Book;
 import com.damjan.librarymanagementsystem.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author authorByFirstName(String first_name) {
+        return authorRepository.findAuthorByFirst_name(first_name);
+    }
 
     public Author findBySearchInput(String searchInput) {
         String[] words = searchInput.split(" ");
@@ -23,5 +34,9 @@ public class AuthorService {
             // Handle other cases if needed
             return null;
         }
+    }
+
+    public List<Book> findBookbyAuthor(Author author) {
+        return authorRepository.findBooksByAuthor(author);
     }
 }
